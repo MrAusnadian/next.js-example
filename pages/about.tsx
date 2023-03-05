93172141bd0f2a890da2b128765d2e21c3ceb489
Will Ryan <9650258+MrAusnadian@users.noreply.github.com>
2023-03-05 12:59:48 -0800

    Fix build failure

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
 import Layout from '../components/layout'
 
 export default function About() {
   return (
-    <Layout>
+    <Layout home>
       <Head>
         <title>About</title>
       </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I'm a driven, enthusiastic, executive-level technology professional with almost two decades of experience working with computer hardware and software. His primary focus is on building, testing, maintaining, and integrating infrastructure for e-commerce and cryptocurrency platform solutions.</p>
+        <p>I&apos;m a driven, enthusiastic, executive-level technology professional with almost two decades of experience working with computer hardware and software. His primary focus is on building, testing, maintaining, and integrating infrastructure for e-commerce and cryptocurrency platform solutions.</p>
       </section>
     </Layout>
   );
