f2db5058c1efcd3e27e4a20ed7b820fc9f78a0d4
Will Ryan <9650258+MrAusnadian@users.noreply.github.com>
2023-03-05 12:54:28 -0800

    Add shared layout, profile image, nav

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import Layout from '../components/layout'
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
 
-export default function About() {
+export default function FirstPost() {
   return (
     <Layout>
       <Head>
-        <title>About</title>
+        <title>First Post</title>
       </Head>
-      <h1>About</h1>
+      <h1>First Post</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed turpis vel tortor iaculis mattis at quis urna. Maecenas interdum magna quis lectus mollis, id sollicitudin magna faucibus. Suspendisse potenti. Nullam ac quam id metus tincidunt imperdiet vitae vitae purus. Aliquam condimentum ultrices ligula. Curabitur leo libero, malesuada at orci non, aliquet iaculis velit. Pellentesque luctus urna quis purus imperdiet, eu tristique erat pharetra. Phasellus et nibh aliquet, dignissim diam quis, molestie neque. Pellentesque ultricies venenatis quam, quis mattis dui malesuada aliquam. Duis fringilla eget nisl sit amet egestas. Vestibulum mauris velit, venenatis a volutpat eu, malesuada sed magna. Morbi ultrices turpis nisl, et finibus mauris dapibus et. In dapibus neque nulla, convallis ullamcorper est ullamcorper sed. Suspendisse sagittis magna eget magna vestibulum fringilla.</p>
       
       <p>Donec feugiat orci sed ligula sollicitudin sagittis. Maecenas nec scelerisque nunc, porttitor ornare nunc. Nullam pharetra nec augue eget mattis. In in ipsum in risus vulputate interdum a nec risus. Integer convallis tempus est, eget consequat eros vehicula id. Praesent quis urna dui. Aliquam vel sapien a nibh convallis fringilla.</p>
